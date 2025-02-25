1c70105abcdaa0ba7397b040764ed51c878734c2
ashley <126aperezmorales@frhsd.com>
2025-02-25 14:00:49 -0500

    first commit

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let lastrenderTime = 0
+const SNAKE_SPEED = 1
+
+function main( currentTime){
+    window.requestAnimationFrame(main)
+    const secondSincelastRender = (currentTime  = lastRenderTime)  / 1000
+}
